ba7871ce053f605f7f58763c70c5d402776377d7
Maria Guerra <maria.gs8@hotmail.es>
2023-09-27 18:00:02 +0100

    :test_tube: The password validator considers a password to be valid when it meets all requirements

---
+++ src/tests/passwordValidator.test.ts
@@ -0,0 +1,7 @@
+
+describe('The password validator', () => {
+	it('considers a password to be valid when it meets all requirements', () => {
+
+		expect(isValidPassword('g0fi_G33ks')).toBe(true);
+	});
+});
