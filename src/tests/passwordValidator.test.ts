af29149306fec79e063218c18debf78eaed7b1bf
Maria Guerra <maria.gs8@hotmail.es>
2023-09-27 18:32:28 +0100

    :test_tube: The password validator considers a password to be invalid when it has less than 8 characters

--- src/tests/passwordValidator.test.ts
+++ src/tests/passwordValidator.test.ts
@@ -1,9 +1,11 @@
-import { isValidPassword } from "../passwordValidator";
-
+import { isValidPassword } from '../passwordValidator';
 
 describe('The password validator', () => {
 	it('considers a password to be valid when it meets all requirements', () => {
-
 		expect(isValidPassword('g0fi_G33ks')).toBe(true);
+	});
+
+	it('considers a password to be invalid when it has less than 8 characters', () => {
+		expect(isValidPassword('Sp3gc_')).toBe(false);
 	});
 });
